2974643c1f1d0252f134430d922c8d9d6af54194
Gideon5348 <gideonmutai096@gmail.com>
2024-06-18 13:04:23 +0000

    12. Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 
-from typing import List, Tuple, Union
+from typing import List, Tuple
 
-
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Returns a list where each item in the tuple lst is repeated factor times.
 
     Parameters:
-    lst (Tuple[int, ...]): A tuple of integers.
+    lst (Tuple): A tuple of elements.
     factor (int): The number of times to repeat each item.
 
     Returns:
-    List[int]: A list with each item repeated factor times.
+    List: A list with each item repeated factor times.
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for _ in range(factor)
     ]
